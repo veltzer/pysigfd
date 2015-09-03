787b3c56fff64cce065249d01265208fe75dc9aa
Mark Veltzer <mark.veltzer@gmail.com>
2015-09-03 09:42:17 +0300

    module distribution work

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 import setuptools # for setup
+import sys # for version_info, exit
+
+if sys.version_info<(3,):
+	print('This module is a python 3 module only')
+	sys.exit(1)
 
 setuptools.setup(
 	install_requires=[
@@ -7,5 +12,12 @@ setuptools.setup(
 	version = '1.1',
 	name = 'sigfd',
 	py_modules=['sigfd'],
-#	packages = ['signalfd'],
+
+	# my details
+	author='Mark Veltzer',
+	author_email = 'mark.veltzer@gmail.com',
+	description = 'This is a python signalfd interface module',
+	license = 'PSF',
+	keywords = 'signalfd python3 linux',
+	url = 'https://github.com/veltzer/python-sigfd', 
 )
