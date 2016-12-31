e89af767b579ada287c9140586fe50517ac4d4d1
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-31 10:30:05 +0200

    typo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     description = 'pysigfd is a module to help you deal with signals within python using the linux signal file description construct',
     long_descriptioni = pypandoc.convert(('README.md', 'rst'),
     url = 'https://github.com/veltzer/pysigfd', 
-    author =i 'Mark Veltzer',
+    author = 'Mark Veltzer',
     author_email = 'mark.veltzer@gmail.com',
     license = 'PSF',
     classifiers = [
