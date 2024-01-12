2ba1390e9e0ced2959d3d38efab55395e57ce719
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-12 15:07:27 +0200

    more python.py changes

--- config/python.py
+++ config/python.py
@@ -10,7 +10,7 @@ config_requires: List[str] = [
 install_requires: List[str] = [
     "cffi",
 ]
-make_requires: List[str] = [
+build_requires: List[str] = [
     "pymakehelper",
     "pydmt",
     "types-cffi",
@@ -23,4 +23,4 @@ test_requires: List[str] = [
     "flake8",
     "mypy",
 ]
-requires = config_requires + install_requires + make_requires + test_requires
+requires = config_requires + install_requires + build_requires + test_requires
