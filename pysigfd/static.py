ebb4e8d944e2b1ff4ad74aa4f4d97bb6713f683c
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-28 21:46:23 +0300

    more

---
+++ pysigfd/static.py
@@ -0,0 +1,5 @@
+""" version which can be consumed from within the module """
+VERSION_STR = "1.3.7"
+DESCRIPTION = "linux signal file descriptor for python"
+APP_NAME = "pysigfd"
+LOGGER_NAME = "pysigfd"
