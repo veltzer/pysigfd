c47950ceb5613984967198825783b880508a638a
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-29 09:33:57 +0300

    fixed up deps

--- pysigfd/static.py
+++
@@ -1,5 +0,0 @@
-""" version which can be consumed from within the module """
-VERSION_STR = "1.3.7"
-DESCRIPTION = "linux signal file descriptor for python"
-APP_NAME = "pysigfd"
-LOGGER_NAME = "pysigfd"
